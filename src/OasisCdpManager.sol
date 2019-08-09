f69ef4ab696cad929bbf76379159b0c8aab57af5
Krzysztof Kostałkowicz <krzysztof.kostalkowicz@in4mates.com>
2019-08-09 14:45:06 +0200

    Remove probably unnecessary code

--- src/OasisCdpManager.sol
+++ src/OasisCdpManager.sol
@@ -20,25 +20,16 @@ contract UrnHandler {
 contract OasisCdpManager is DSNote {
     address                   public vat;
 
-    mapping (address => address) public owns;   // UrnHandler => Owner
     mapping (
         address => mapping (
             bytes32 => address
         )
     ) public urns;                              // Owner => Ilk => UrnHandler
 
-    mapping (
-        address => mapping (
-            address => mapping (
-                address => uint
-            )
-        )
-    ) public allows;                            // Owner => UrnHandler => Allowed Addr => True/False
-
     event NewCdp(address indexed usr, address indexed own, bytes32 ilk, address urn);
 
-    modifier isAllowed(address urn) {
-        require(msg.sender == owns[urn] || allows[owns[urn]][urn][msg.sender] == 1, "not-allowed");
+    modifier isAllowed(address usr) {
+        require(msg.sender == usr, "not-allowed");
         _;
     }
 
@@ -46,71 +37,32 @@ contract OasisCdpManager is DSNote {
         vat = vat_;
     }
 
-    function add(uint x, uint y) internal pure returns (uint z) {
-        require((z = x + y) >= x, "add-overflow");
-    }
-
-    function sub(uint x, uint y) internal pure returns (uint z) {
-        require((z = x - y) <= x, "sub-overflow");
-    }
-
-    function toInt(uint x) internal pure returns (int y) {
-        y = int(x);
-        require(y >= 0, "uint-to-int-overflow");
-    }
-
-    // Allow/disallow a dst address to manage the cdp.
-    function allow(
-        address urn,
-        address usr,
-        uint ok
-    ) public {
-        allows[msg.sender][urn][usr] = ok;
-    }
-
     // Open a new cdp for the caller.
-    function open(bytes32 ilk) public returns (address) {
-        return open(ilk, msg.sender);
+    function open(bytes32 ilk) public {
+        open(ilk, msg.sender);
     }
 
     // Open a new cdp for a given usr address.
     function open(
         bytes32 ilk,
         address usr
-    ) public note returns (address urn) {
+    ) public note {
         require(usr != address(0), "usr-address-0");
         require(urns[usr][ilk] == address(0), "cannot-override-urn");
 
-        urn = address(new UrnHandler(vat));
+        address urn = address(new UrnHandler(vat));
         urns[usr][ilk] = urn;
-        owns[urn] = usr;
         emit NewCdp(msg.sender, usr, ilk, urn);
-    }
-
-    // Give the cdp ownership to a dst address.
-    function give(
-        address urn,
-        address dst,
-        bytes32 ilk
-    ) public note isAllowed(urn) {
-        address owner = owns[urn];
-        require(dst != address(0), "dst-address-0");
-        require(dst != owns[urn], "dst-already-owner");
-        require(urns[owner][ilk] == urn, "invalid-ilk-value");
-        require(urns[dst][ilk] == address(0), "cannot-override-dst-urn");
-
-        owns[urn] = dst;
-        urns[dst][ilk] = urn;
-        delete urns[owner][ilk];
     }
 
     // Frob the cdp keeping the generated DAI or collateral freed in the cdp urn address.
     function frob(
-        address urn,
+        address usr,
         bytes32 ilk,
         int dink,
         int dart
-    ) public note isAllowed(urn) {
+    ) public note isAllowed(usr) {
+        address urn = urns[usr][ilk];
         VatLike(vat).frob(
             ilk,
             urn,
@@ -121,56 +73,4 @@ contract OasisCdpManager is DSNote {
         );
     }
 
-    // Frob the cdp sending the generated DAI or collateral freed to a dst address.
-    function frob(
-        address urn,
-        address dst,
-        bytes32 ilk,
-        int dink,
-        int dart
-    ) public note isAllowed(urn) {
-        VatLike(vat).frob(
-            ilk,
-            urn,
-            dink >= 0 ? urn : dst,
-            dart <= 0 ? urn : dst,
-            dink,
-            dart
-        );
-    }
-
-    // Transfer wad amount of cdp collateral from the cdp address to a dst address.
-    function flux(
-        address urn,
-        address dst,
-        bytes32 ilk,
-        uint wad
-    ) public note isAllowed(urn) {
-        VatLike(vat).flux(ilk, urn, dst, wad);
-    }
-
-    // Transfer wad amount of DAI from the cdp address to a dst address.
-    function move(
-        address urn,
-        address dst,
-        uint rad
-    ) public note isAllowed(urn) {
-        VatLike(vat).move(urn, dst, rad);
-    }
-
-    // Quit the system, migrating the cdp (ink, art) to a different dst urn
-    function quit(
-        address urn,
-        address dst,
-        bytes32 ilk
-    ) public note isAllowed(urn) {
-        (uint ink, uint art) = VatLike(vat).urns(ilk, urn);
-        VatLike(vat).fork(
-            ilk,
-            urn,
-            dst,
-            toInt(ink),
-            toInt(art)
-        );
-    }
 }
--- src/OasisCdpManager.t.sol
+++ src/OasisCdpManager.t.sol
@@ -1,49 +1,44 @@
 pragma solidity >= 0.5.0;
 
 import { DssDeployTestBase } from "dss-deploy/DssDeploy.t.base.sol";
-import "./OasisCdpManager.sol";
+import "./TestableOasisCdpManager.sol";
 
 contract FakeUser {
-    function doGive(
-        OasisCdpManager manager,
-        address urn,
-        address dst,
-        bytes32 ilk
-    ) public {
-        manager.give(urn, dst, ilk);
-    }
-
     function doFrob(
         OasisCdpManager manager,
-        address urn,
+        address usr,
         bytes32 ilk,
         int dink,
         int dart
     ) public {
-        manager.frob(urn, ilk, dink, dart);
+        manager.frob(usr, ilk, dink, dart);
     }
 }
 
 contract OasisCdpManagerTest is DssDeployTestBase {
-    OasisCdpManager manager;
+    TestableOasisCdpManager manager;
     FakeUser user;
 
     function setUp() public {
         super.setUp();
         deploy();
-        manager = new OasisCdpManager(address(vat));
+        manager = new TestableOasisCdpManager(address(vat));
         user = new FakeUser();
     }
 
     function testOpenCDP() public {
-        address cdp = manager.open("ETH");
+        manager.open("ETH");
         assertEq(vat.can(manager.urns(address(this), "ETH"), address(manager)), 1);
-        assertEq(manager.owns(cdp), address(this));
+        address urn = manager.urns(address(this), "ETH");
+        if (urn == address(0))
+            fail();
     }
 
     function testOpenCDPOtherAddress() public {
-        address cdp = manager.open("ETH", address(123));
-        assertEq(manager.owns(cdp), address(123));
+        manager.open("ETH", address(123));
+        address urn = manager.urns(address(123), "ETH");
+        if (urn == address(0))
+            fail();
     }
 
     function testFailOpenCDPZeroAddress() public {
@@ -55,87 +50,15 @@ contract OasisCdpManagerTest is DssDeployTestBase {
         manager.open("ETH", address(123));
     }
 
-    function testGiveCDP() public {
-        address cdp = manager.open("ETH");
-        manager.give(cdp, address(123), "ETH");
-        assertEq(manager.owns(cdp), address(123));
-    }
-
-    function testGiveAllowed() public {
-        address cdp = manager.open("ETH");
-        manager.allow(cdp, address(user), 1);
-        user.doGive(manager, cdp, address(123), "ETH");
-        assertEq(manager.owns(cdp), address(123));
-    }
-
-    function testGiveManyTimes() public {
-        address cdp = manager.open("ETH");
-        manager.give(cdp, address(user), "ETH");
-        user.doFrob(manager, cdp, "ETH", 0, 0);
-        user.doGive(manager, cdp, address(this), "ETH");
-        manager.frob(cdp, "ETH", 0, 0);
-    }
-
-    function testFailGiveNotAllowed() public {
-        address cdp = manager.open("ETH");
-        user.doGive(manager, cdp, address(123), "ETH");
-    }
-
-    function testFailGiveNotAllowed2() public {
-        address cdp = manager.open("ETH");
-        manager.allow(cdp, address(user), 1);
-        manager.allow(cdp, address(user), 0);
-        user.doGive(manager, cdp, address(123), "ETH");
-    }
-
-    function testFailGiveNotAllowed3() public {
-        address cdp = manager.open("ETH");
-        address cdp2 = manager.open("ETH");
-        manager.allow(cdp2, address(user), 1);
-        user.doGive(manager, cdp, address(123), "ETH");
-    }
-
-    function testFailGiveToZeroAddress() public {
-        address cdp = manager.open("ETH");
-        manager.give(cdp, address(0), "ETH");
-    }
-
-    function testFailGiveToSameOwner() public {
-        address cdp = manager.open("ETH");
-        manager.give(cdp, address(this), "ETH");
-    }
-
-    function testFailInvalidIlk() public {
-        address cdp = manager.open("ETH");
-        manager.give(cdp, address(123), "ZZZ");
-    }
-
-    function testFailOverride() public {
-        address cdp = manager.open("ETH");
-        manager.open("ETH", address(123));
-        manager.give(cdp, address(123), "ETH");
-    }
-
-    function testFailGiveResetAllowances() public {
-        FakeUser tester = new FakeUser();
-        address cdp = manager.open("ETH");
-
-        manager.allow(cdp, address(tester), 1);
-        tester.doFrob(manager, cdp, "ETH", 0, 0);
-
-        manager.give(cdp, address(user), "ETH");
-        tester.doFrob(manager, cdp, "ETH", 0, 0);
-    }
-
     function testFrob() public {
-        address cdp = manager.open("ETH");
+        manager.open("ETH");
         weth.deposit.value(1 ether)();
         weth.approve(address(ethJoin), 1 ether);
         ethJoin.join(manager.urns(address(this), "ETH"), 1 ether);
-        manager.frob(cdp, "ETH", 1 ether, 50 ether);
+        manager.frob(address(this), "ETH", 1 ether, 50 ether);
         assertEq(vat.dai(manager.urns(address(this), "ETH")), 50 ether * ONE);
         assertEq(vat.dai(address(this)), 0);
-        manager.move(cdp, address(this), 50 ether * ONE);
+        manager.move(address(this), "ETH", address(this), 50 ether * ONE);
         assertEq(vat.dai(manager.urns(address(this), "ETH")), 0);
         assertEq(vat.dai(address(this)), 50 ether * ONE);
         assertEq(dai.balanceOf(address(this)), 0);
@@ -145,57 +68,47 @@ contract OasisCdpManagerTest is DssDeployTestBase {
     }
 
     function testFrobDaiOtherDst() public {
-        address cdp = manager.open("ETH");
+        manager.open("ETH");
         weth.deposit.value(1 ether)();
         weth.approve(address(ethJoin), 1 ether);
         ethJoin.join(manager.urns(address(this), "ETH"), 1 ether);
-        manager.frob(cdp, address(this), "ETH", 1 ether, 50 ether);
+        manager.frob(address(this), "ETH", address(this), 1 ether, 50 ether);
         assertEq(vat.dai(manager.urns(address(this), "ETH")), 0);
         assertEq(vat.dai(address(this)), 50 ether * ONE);
     }
 
     function testFrobGemOtherDst() public {
-        address cdp = manager.open("ETH");
+        manager.open("ETH");
         weth.deposit.value(1 ether)();
         weth.approve(address(ethJoin), 1 ether);
         ethJoin.join(manager.urns(address(this), "ETH"), 1 ether);
-        manager.frob(cdp, "ETH", 1 ether, 50 ether);
+        manager.frob(address(this), "ETH", 1 ether, 50 ether);
         assertEq(vat.gem("ETH", manager.urns(address(this), "ETH")), 0);
         assertEq(vat.gem("ETH", address(this)), 0);
-        manager.frob(cdp, address(this), "ETH", -int(1 ether), -int(50 ether));
+        manager.frob(address(this), "ETH", address(this), -int(1 ether), -int(50 ether));
         assertEq(vat.gem("ETH", manager.urns(address(this), "ETH")), 0);
         assertEq(vat.gem("ETH", address(this)), 1 ether);
     }
 
-    function testFrobAllowed() public {
-        address cdp = manager.open("ETH");
-        weth.deposit.value(1 ether)();
-        weth.approve(address(ethJoin), 1 ether);
-        ethJoin.join(manager.urns(address(this), "ETH"), 1 ether);
-        manager.allow(cdp, address(user), 1);
-        user.doFrob(manager, cdp, "ETH", 1 ether, 50 ether);
-        assertEq(vat.dai(manager.urns(address(this), "ETH")), 50 ether * ONE);
-    }
-
     function testFailFrobNotAllowed() public {
-        address cdp = manager.open("ETH");
+        manager.open("ETH");
         weth.deposit.value(1 ether)();
         weth.approve(address(ethJoin), 1 ether);
         ethJoin.join(manager.urns(address(this), "ETH"), 1 ether);
-        user.doFrob(manager, cdp, "ETH", 1 ether, 50 ether);
+        user.doFrob(manager, address(this), "ETH", 1 ether, 50 ether);
     }
 
     function testFrobGetCollateralBack() public {
-        address cdp = manager.open("ETH");
+        manager.open("ETH");
         weth.deposit.value(1 ether)();
         weth.approve(address(ethJoin), 1 ether);
         ethJoin.join(manager.urns(address(this), "ETH"), 1 ether);
-        manager.frob(cdp, "ETH", 1 ether, 50 ether);
-        manager.frob(cdp, "ETH", -int(1 ether), -int(50 ether));
+        manager.frob(address(this), "ETH", 1 ether, 50 ether);
+        manager.frob(address(this), "ETH", -int(1 ether), -int(50 ether));
         assertEq(vat.dai(address(this)), 0);
         assertEq(vat.gem("ETH", manager.urns(address(this), "ETH")), 1 ether);
         assertEq(vat.gem("ETH", address(this)), 0);
-        manager.flux(cdp, address(this), "ETH", 1 ether);
+        manager.flux(address(this), "ETH", address(this), 1 ether);
         assertEq(vat.gem("ETH", manager.urns(address(this), "ETH")), 0);
         assertEq(vat.gem("ETH", address(this)), 1 ether);
         uint prevBalance = address(this).balance;
@@ -204,39 +117,4 @@ contract OasisCdpManagerTest is DssDeployTestBase {
         assertEq(address(this).balance, prevBalance + 1 ether);
     }
 
-    function testGetWrongCollateralBack() public {
-        address cdp = manager.open("ETH");
-        col.mint(1 ether);
-        col.approve(address(colJoin), 1 ether);
-        colJoin.join(manager.urns(address(this), "ETH"), 1 ether);
-        assertEq(vat.gem("COL", manager.urns(address(this), "ETH")), 1 ether);
-        assertEq(vat.gem("COL", address(this)), 0);
-        manager.flux(cdp, address(this), "COL", 1 ether);
-        assertEq(vat.gem("COL", manager.urns(address(this), "ETH")), 0);
-        assertEq(vat.gem("COL", address(this)), 1 ether);
-    }
-
-    function testQuit() public {
-        address cdp = manager.open("ETH");
-        weth.deposit.value(1 ether)();
-        weth.approve(address(ethJoin), 1 ether);
-        ethJoin.join(manager.urns(address(this), "ETH"), 1 ether);
-        manager.frob(cdp, "ETH", 1 ether, 50 ether);
-
-        (uint ink, uint art) = vat.urns("ETH", manager.urns(address(this), "ETH"));
-        assertEq(ink, 1 ether);
-        assertEq(art, 50 ether);
-        (ink, art) = vat.urns("ETH", address(this));
-        assertEq(ink, 0);
-        assertEq(art, 0);
-
-        vat.hope(address(manager));
-        manager.quit(cdp, address(this), "ETH");
-        (ink, art) = vat.urns("ETH", manager.urns(address(this), "ETH"));
-        assertEq(ink, 0);
-        assertEq(art, 0);
-        (ink, art) = vat.urns("ETH", address(this));
-        assertEq(ink, 1 ether);
-        assertEq(art, 50 ether);
-    }
 }
